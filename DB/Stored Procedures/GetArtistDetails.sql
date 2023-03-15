3a0744093a755e34246e8eb655d8535913ab09ab
Olorunfemi <oayanfe@yanot.ng>
2023-03-15 18:25:13 +0100

    commit change

--- DB/Stored Procedures/GetArtistDetails.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[GetArtistDetails]
-	@artistID INT = -1
-	
-AS
-BEGIN
-	SELECT * FROM [dbo].Artist WHERE artistID = @artistID
-
-END
---
+++ DB/Stored Procedures/spGetArtistDetails1.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE dbo.spGetArtistDetails1
+	@artistID int = 1
+AS
+BEGIN
+	
+    -- Insert statements for procedure here
+ SELECT Artist.title, Artist.biography, Artist.imageURL, Artist.heroURL,Song.songID, Song.title, Song.bpm,Album.albumID, Album.title, Album.imageURL
+ FROM Artist JOIN Song ON Artist.artistID= Song.artistID JOIN Album ON Artist.artistID = Album.artistID WHERE Artist.artistID = @artistID
+END
